1cdde2b08933977dfdf8a406942748a34dc4545d
tommy.murphy <tommy.murphy@cbfd5c61-442b-0410-9734-bdb964165c8a>
2007-03-18 20:05:28 +0000

    Added GPL notices

--- trunk/jShout/src/com/ourbunny/jshout/Shouter.java
+++ trunk/jShout/src/com/ourbunny/jshout/Shouter.java
@@ -1,14 +1,30 @@
-/**
+/*
  * 
+ *	=======================================================================
+ * 	jShout - Stream audio from your program to an (ice/shout)cast server
+ *	Copyright (C) 2007  Tommy Murphy
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License along
+ *	with this program; if not, write to the Free Software Foundation, Inc.,
+ *	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *	=======================================================================
+ *
  */
+
 package com.ourbunny.jshout;
 
 import java.net.*;
 
-/**
- * @author tommy
- *
- */
 public class Shouter{
 	// stream config info
 	private StreamConfig config;
@@ -17,9 +33,6 @@ public class Shouter{
 	private StreamSocket mysocket;
 	private boolean connected = false;
 	
-	/**
-	 * 
-	 */
 	public Shouter(StreamConfig c) {
 		this.config = c;
 	}
--- trunk/jShout/src/com/ourbunny/jshout/StreamSocket.java
+++ trunk/jShout/src/com/ourbunny/jshout/StreamSocket.java
@@ -1,8 +1,25 @@
 /*
- * Parts of this file are copied from JRoar
- * Copyright (C) 2001,2002 ymnk, JCraft,Inc.
- * 
- * Licensed under the GNU General Public License
+ *	Parts of this file are bassed off of the JRoar source code... GPL rocks
+ *	Copyright (C) 2001,2002 ymnk, JCraft,Inc.
+ *	=======================================================================
+ * 	jShout - Stream audio from your program to an (ice/shout)cast server
+ *	Copyright (C) 2007  Tommy Murphy
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License along
+ *	with this program; if not, write to the Free Software Foundation, Inc.,
+ *	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *	=======================================================================
+ *
  */
 
 package com.ourbunny.jshout;
